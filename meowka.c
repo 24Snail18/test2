1847e4d353faa034157ba2423d3f4b24a08f1c51
Nikita Ilyin <nekit.2002.2002@gmail.com>
2021-07-08 15:11:38 +0300

    first commit

---
+++ meowka.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("top programmist\n");
+    int a;
+    int b;
+
+    a=21;
+    b=2002;
+    printf("%d hello %d\n", a,b);
+    a=b;
+    printf("my birth year is %d\n", a);
+
+    char                c;
+    float               f;
+    unsigned int        i;
+    double              d;
+
+    printf("size is %lu\n",sizeof(i));
+}
